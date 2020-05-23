1f742670dcb5c69e8115c9b576d8115644275a04
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-05-22 18:47:45 -0600

    Refactor Inbox panel to use user prefs hook.
    @todo - updating last read isn't working.

--- client/header/activity-panel/panels/inbox/index.js
+++ client/header/activity-panel/panels/inbox/index.js
@@ -2,10 +2,14 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component, Fragment } from '@wordpress/element';
+import { Fragment /*, useEffect */ } from '@wordpress/element';
 import { compose } from '@wordpress/compose';
 import Gridicon from 'gridicons';
-import { withDispatch } from '@wordpress/data';
+
+/**
+ * WooCommerce dependencies
+ */
+import { useUserPreferences } from '@woocommerce/data';
 
 /**
  * Internal dependencies
@@ -17,108 +21,103 @@ import { EmptyContent, Section } from '@woocommerce/components';
 import { QUERY_DEFAULTS } from 'wc-api/constants';
 import withSelect from 'wc-api/with-select';
 
-class InboxPanel extends Component {
-	constructor( props ) {
-		super( props );
-		this.mountTime = Date.now();
+const renderEmptyCard = () => (
+	<ActivityCard
+		className="woocommerce-empty-activity-card"
+		title={ __( 'Your inbox is empty', 'woocommerce-admin' ) }
+		icon={ <Gridicon icon="checkmark" size={ 48 } /> }
+	>
+		{ __(
+			'As things begin to happen in your store your inbox will start to fill up. ' +
+				"You'll see things like achievements, new feature announcements, extension recommendations and more!",
+			'woocommerce-admin'
+		) }
+	</ActivityCard>
+);
+
+const renderNotes = ( { lastRead, notes } ) => {
+	if ( Object.keys( notes ).length === 0 ) {
+		return renderEmptyCard();
 	}
 
-	componentWillUnmount() {
-		const userDataFields = {
-			activity_panel_inbox_last_read: this.mountTime,
-		};
-		this.props.updateCurrentUserData( userDataFields );
-	}
+	const notesArray = Object.keys( notes ).map( ( key ) => notes[ key ] );
 
-	renderEmptyCard() {
-		return (
-			<ActivityCard
-				className="woocommerce-empty-activity-card"
-				title={ __( 'Your inbox is empty', 'woocommerce-admin' ) }
-				icon={ <Gridicon icon="checkmark" size={ 48 } /> }
-			>
-				{ __(
-					'As things begin to happen in your store your inbox will start to fill up. ' +
-						"You'll see things like achievements, new feature announcements, extension recommendations and more!",
-					'woocommerce-admin'
-				) }
-			</ActivityCard>
+	return notesArray.map( ( note ) => (
+		<InboxNoteCard
+			key={ note.id }
+			note={ note }
+			lastRead={ lastRead }
+		/>
+	) );
+}
+
+const InboxPanel = ( props ) => {
+	const { isError, isRequesting, notes } = props;
+	const { /* updateUserPreferences, */ ...userPrefs } = useUserPreferences();
+	const lastRead = userPrefs.activity_panel_inbox_last_read;
+
+	// @todo - this is apparently NOT the right way to do this!
+	// useEffect( () => {
+	// 	const mountTime = Date.now();
+	// 
+	// 	return () => {
+	// 		console.log( 'updating last read', mountTime );
+	// 		const userDataFields = {
+	// 			activity_panel_inbox_last_read: mountTime,
+	// 		};
+	// 		updateUserPreferences( userDataFields );
+	// 	};
+	// } );
+
+	if ( isError ) {
+		const title = __(
+			'There was an error getting your inbox. Please try again.',
+			'woocommerce-admin'
 		);
-	}
-
-	renderNotes() {
-		const { lastRead, notes } = this.props;
-
-		if ( Object.keys( notes ).length === 0 ) {
-			return this.renderEmptyCard();
-		}
-
-		const notesArray = Object.keys( notes ).map( ( key ) => notes[ key ] );
-
-		return notesArray.map( ( note ) => (
-			<InboxNoteCard
-				key={ note.id }
-				note={ note }
-				lastRead={ lastRead }
-			/>
-		) );
-	}
-
-	render() {
-		const { isError, isRequesting } = this.props;
-
-		if ( isError ) {
-			const title = __(
-				'There was an error getting your inbox. Please try again.',
-				'woocommerce-admin'
-			);
-			const actionLabel = __( 'Reload', 'woocommerce-admin' );
-			const actionCallback = () => {
-				// @todo Add tracking for how often an error is displayed, and the reload action is clicked.
-				window.location.reload();
-			};
-
-			return (
-				<Fragment>
-					<EmptyContent
-						title={ title }
-						actionLabel={ actionLabel }
-						actionURL={ null }
-						actionCallback={ actionCallback }
-					/>
-				</Fragment>
-			);
-		}
+		const actionLabel = __( 'Reload', 'woocommerce-admin' );
+		const actionCallback = () => {
+			// @todo Add tracking for how often an error is displayed, and the reload action is clicked.
+			window.location.reload();
+		};
 
 		return (
 			<Fragment>
-				<ActivityHeader title={ __( 'Inbox', 'woocommerce-admin' ) } />
-				<Section>
-					{ isRequesting ? (
-						<ActivityCardPlaceholder
-							className="woocommerce-inbox-activity-card"
-							hasAction
-							hasDate
-							lines={ 2 }
-						/>
-					) : (
-						this.renderNotes()
-					) }
-				</Section>
+				<EmptyContent
+					title={ title }
+					actionLabel={ actionLabel }
+					actionURL={ null }
+					actionCallback={ actionCallback }
+				/>
 			</Fragment>
 		);
 	}
+
+	return (
+		<Fragment>
+			<ActivityHeader title={ __( 'Inbox', 'woocommerce-admin' ) } />
+			<Section>
+				{ isRequesting ? (
+					<ActivityCardPlaceholder
+						className="woocommerce-inbox-activity-card"
+						hasAction
+						hasDate
+						lines={ 2 }
+					/>
+				) : (
+					renderNotes( { lastRead, notes })
+				) }
+			</Section>
+		</Fragment>
+	);
 }
 
 export default compose(
 	withSelect( ( select ) => {
 		const {
-			getCurrentUserData,
 			getNotes,
 			getNotesError,
 			isGetNotesRequesting,
 		} = select( 'wc-api' );
-		const userData = getCurrentUserData();
 		const inboxQuery = {
 			page: 1,
 			per_page: QUERY_DEFAULTS.pageSize,
@@ -148,14 +147,6 @@ export default compose(
 			notes,
 			isError,
 			isRequesting,
-			lastRead: userData.activity_panel_inbox_last_read,
-		};
-	} ),
-	withDispatch( ( dispatch ) => {
-		const { updateCurrentUserData } = dispatch( 'wc-api' );
-
-		return {
-			updateCurrentUserData,
 		};
 	} )
 )( InboxPanel );
